60388578f9b5d30e876f1e7dd4adc7957861b8a6
Aditya Degwekar <adegwekar@gmail.com>
2014-06-25 21:16:21 -0700

    GasStation: Create library for API

--- GasStation/GasStation.cpp
+++ GasStation/GasStation.cpp
@@ -12,41 +12,50 @@ string GasStation(vector<string> strArr)
   int NumGasStations = atoi(strArr[0].c_str());
   int GasStationsVisited = 0;
   int StartingGasStation = 1;
-  
+
+  // N >= 2
   if (strArr[0] < "2")
     return "impossible";
 
-  while (GasStationsVisited < NumGasStations) {
+  while (1) {
     int pos = strArr[CurrentGasStation+1].find(":");
     string token = strArr[CurrentGasStation+1].substr(0, pos);
     int AvailableGas = atoi(token.c_str());
-    int GasNeeded = atoi(strArr[CurrentGasStation+1].substr(pos+1,strArr[CurrentGasStation+1].size()).c_str());
+    int GasNeeded =
+      atoi(strArr[CurrentGasStation+1].substr(pos+1,
+                                  strArr[CurrentGasStation+1].size()).c_str());
     
     if (CurrentGasLevel+AvailableGas >= GasNeeded) {
-      CurrentGasLevel += AvailableGas;
-      CurrentGasLevel -= GasNeeded;
+      // Enough gas to proceed
+      CurrentGasLevel += AvailableGas; // Filled up gas
+      CurrentGasLevel -= GasNeeded; // Used up gas
       ++GasStationsVisited;
     } else {
+      // Not enough gas to proceed, but we can always try the next stations
+      // Unless...
+      // We have already circled through all stations once, then return impossible
       if (GasStationsVisited >= CurrentGasStation &&
           StartingGasStation-1 > CurrentGasStation) {
         return "impossible";
       }
       GasStationsVisited = 0;
       CurrentGasLevel = 0;
+      // Try the next station (extra +1 to align with array indices)
       StartingGasStation = CurrentGasStation+1+1;
     }
-    ++CurrentGasStation;
+    ++CurrentGasStation; // Check next station
     
     if (GasStationsVisited == NumGasStations) {
-      break;
+      break; // Done
     }
     
     // Reached the end, should we circle back?
     if (CurrentGasStation >= NumGasStations) {
       if (GasStationsVisited == 0) {
+        // No gas stations were able to satisfy the need
         return "impossible";
       } else {
-        //Not done with all gas stations
+        //Start back at the beginning, maybe we might have enough gas to circle
         CurrentGasStation = 0;
       }
     }
